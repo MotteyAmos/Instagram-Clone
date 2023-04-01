df7e917a29b6614e1ea55e53c1fb8a1024f765aa
Router <akamottey@st.ug.edu.gh>
2023-04-01 04:58:14 +0000

    added instagram image to repace text and some stylings

--- LoginPage/src/App.jsx
+++ LoginPage/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css';
 import { FaFacebookSquare } from "react-icons/fa";
 import googlePlay from "./assets/googlePlay.png";
 import microsoft from "./assets/microsoft.png";
+import instagramTitle from "./assets/instagram_title.jpg";
+import Footer from './assets/components/footer';
 
 export default function App() {
   return (
@@ -13,7 +15,9 @@ export default function App() {
         <div className="container position-absolute mw-100 w-25 instagram-card">
           <div className="card mw-100 w-100 text-center" style={{ width: '18rem' }}>
             <div className="card-body">
-              <h5 className="card-title form-title">Instagram</h5>
+              <h5 className="card-title form-title">
+                <img src={instagramTitle} alt="instagram Title" />
+              </h5>
               <form >
                 <div className='container position-relative  form-container' >
                   <input type="text" placeholder='Phone number, username or email address' className="text-muted border" />
@@ -50,43 +54,8 @@ export default function App() {
         </div>
 
       </div>
-      <footer className="container">
-        <ol>
-          <li><a href="#">Meta</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Blog</a></li>
-          <li><a href="#">Jobs</a></li>
-          <li><a href="#">Help</a></li>
-          <li><a href="#">API</a></li>
-          <li><a href="#">Privacy</a></li>
-          <li><a href="#">Terms</a></li>
-          <li><a href="#">Top accounts</a></li>
-          <li><a href="#">Locations</a></li>
-          <li><a href="#">Instagram Lite</a></li>
-          <li><a href="#">Contact uploading and non-users</a></li>
-          <li><a href="#">Meta Verified</a></li>
-          <li>
-            <select class="form-select select" aria-label="Default select example ">
-              <option selected>English(UK)</option>
-              <option value="Dask">Dask</option>
-              <option value="Suomi">Suomi</option>
-              <option value="Portugues(Brasil)">Portugues(Brasil)</option>
-              <option value="Dansk">Dansk</option>
-              <option value="Afrikaans">Afrkaans</option>
-              <option value="Deutsch">Cestina</option>
-              <option value="Espanol">Espanol</option>
-              <option value="Bahasa Melayu">Bahasa Melayu</option>
-              <option value="Portugues(Brasil)">Portugues (Brasil)</option>
-              <option value="Portugues(Portugal)">Portugues(Portugal)</option>
-            </select>
-          </li>
-          <li>	<a href="#"> &#169; 2023 Instagram from Meta</a></li>
-
-
-
-        </ol>
-      </footer>
-
+      
+      <Footer/>
     </>
   )
 }
---
+++ LoginPage/src/assets/components/footer.jsx
@@ -0,0 +1,45 @@
+
+
+export default function Footer(){
+    return(
+        <>
+            <footer className="container">
+        <ol>
+          <li><a href="#">Meta</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Jobs</a></li>
+          <li><a href="#">Help</a></li>
+          <li><a href="#">API</a></li>
+          <li><a href="#">Privacy</a></li>
+          <li><a href="#">Terms</a></li>
+          <li><a href="#">Top accounts</a></li>
+          <li><a href="#">Locations</a></li>
+          <li><a href="#">Instagram Lite</a></li>
+          <li><a href="#">Contact uploading and non-users</a></li>
+          <li><a href="#">Meta Verified</a></li>
+          <li>
+            <select class="form-select select" aria-label="Default select example ">
+              <option selected>English(UK)</option>
+              <option value="Dask">Dask</option>
+              <option value="Suomi">Suomi</option>
+              <option value="Portugues(Brasil)">Portugues(Brasil)</option>
+              <option value="Dansk">Dansk</option>
+              <option value="Afrikaans">Afrkaans</option>
+              <option value="Deutsch">Cestina</option>
+              <option value="Espanol">Espanol</option>
+              <option value="Bahasa Melayu">Bahasa Melayu</option>
+              <option value="Portugues(Brasil)">Portugues (Brasil)</option>
+              <option value="Portugues(Portugal)">Portugues(Portugal)</option>
+            </select>
+          </li>
+          <li>	<a href="#"> &#169; 2023 Instagram from Meta</a></li>
+
+
+
+        </ol>
+      </footer>
+        
+        </>
+    )
+}
